02becff54aa57bdc566ae9b40ef4261cc9a164c2
Jason Rowley <jasonrowley@verizon.net>
2017-11-14 16:22:23 -0500

    cleaning graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -49,37 +49,11 @@ public class Graph {
             e.printStackTrace();
         }
         return vertices;
-
     }
+
+    // // preform color-dfs on graph  
+    // public boolean dfs(Vertex[] vertices) {
+        
+    // }
+
 }
-
-// void dfs(G, V, E):
-//     System.out.println("hello world")
-
-// def generate_graph(filename):
-//     """
-//     Takes a filename, reads the file, initializing a dictionary keyed by vertex_id that stores the adjacency list for that node
-
-//     :param filename: name of the input file with the number of vertices and the enumerated, undirected edges
-//     :return: ********************************************************************************************************************************** TODO what
-//     """
-//     graph = []
-
-//     with open(filename) as file:
-//         # get the number of vertices in graph
-//         num_vertices = file.__next__()
-
-//         for line in file:
-//             i = 0
-//             # print(line)
-
-// if __name__ == '__main__':
-//     import argparse
-
-//     parser = argparse.ArgumentParser(description='Returns if a undirected graph is two colorable')
-//     parser.add_argument('filename', help='name of the input file with number graph information')
-
-//     args = parser.parse_args()
-//     generate_graph(args.filename)
-
-
