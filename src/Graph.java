86063e16a1a1b0f60c334a9752f50636b91d29e8
Jason Rowley <jasonrowley@verizon.net>
2017-11-14 01:03:26 -0500

    initialized edge lists for vertices

--- src/Graph.java
+++ src/Graph.java
@@ -20,11 +20,30 @@ public class Graph {
         try { 
             BufferedReader reader = new BufferedReader(new FileReader(filename));
             int num_vertices = Integer.parseInt(reader.readLine());
-            vertices = new Vertex[num_vertices];
+            vertices = new Vertex[num_vertices+1];
+            // initialize vertices 1 -> num_vertices
+            for(int i = 1; i <= num_vertices; i++) {
+                ArrayList<Integer> edge_list = new ArrayList<Integer>();
+                vertices[i] = new Vertex((i+1), edge_list);
+            }
+
             String line = null; 
             while((line = reader.readLine()) != null) {
+                String[] edge = line.split(" ");
+                int first = Integer.parseInt(edge[0]);
+                int second = Integer.parseInt(edge[1]);
+                // add first vertex to second vertex adjacency list
+                vertices[first].edges.add(second);
+
+                // add second vertex to first vertex adjacency list
+                vertices[second].edges.add(first);
+
                 System.out.println(line);
             }
+            for(int i = 1; i < vertices.length; i++){
+                System.out.println(vertices[i].edges);
+            }
+
         }
         catch(Exception e) { 
             e.printStackTrace();
